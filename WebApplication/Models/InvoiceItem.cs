1281a3eb6658a6fee057aecf0a564cc7d9622cad
bradley augstein <bradley.augstein@finastra.co.uk>
2023-11-09 08:35:55 +0000

    INGE-2 - Localhost app with invoice properties

---
+++ WebApplication/Models/InvoiceItem.cs
@@ -0,0 +1,13 @@
+﻿namespace InvoiceGenerator.Models
+{
+    public class InvoiceItem
+    {
+        public long Id { get; set; }
+        public string Date { get; set; }
+        public string Name { get; set; }
+        public double Amount { get; set; }
+        public string Address { get; set; }
+        public string Type { get; set; }
+        public string Email { get; set; }
+    }
+}
